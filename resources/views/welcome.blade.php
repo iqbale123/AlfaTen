4937b664e295507fd9e807fb70dfe3ccbd90e065
iqbale123 <imuflishin@gmail.com>
2022-09-12 11:07:07 +0700

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -119,6 +119,7 @@
                             <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
                                 Sponsor
                             </a>
+                            <button>button</button>
                         </div>
                     </div>
 
